5c5fcbe7cdc554a3d7e358bcd1a893268dd95cd4
Claudio Gael Rodriguez Vega <kawigael99@gmail.cpm>
2026-03-10 18:07:29 -0600

    semana 8

--- intento_gui/src/controlador/ControladorVistaVentana.java
+++ intento_gui/src/controlador/ControladorVistaVentana.java
@@ -1,18 +1,119 @@
 package controlador;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+
 import vista.VistaVentana;
 
-public class ControladorVistaVentana {
-    VistaVentana VistaVentana;
+public class ControladorVistaVentana implements MouseListener, ActionListener{
+    VistaVentana VV;
 
     public ControladorVistaVentana(){
-
+    }
+    
+    
+    public ControladorVistaVentana(VistaVentana VistaVentana) {
+        this.VV = VistaVentana;
+        this.VV.setVisible(true);
+        listeners();
     }
 
 
-    public ControladorVistaVentana(VistaVentana VistaVentana) {
-        this.VistaVentana = VistaVentana;
-        this.VistaVentana.setVisible(true);
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        Object Source = e.getSource();
+        System.out.println(Source);
+        if(Source == this.VV.BtnSalir){
+            System.exit(0);
+        }
+        if(Source == this.VV.BtnVerde){
+            this.VV.BtnVerde.setBackground(Color.green);
+        }
+
+        if(Source == this.VV.BtnRojo){
+            this.VV.BtnRojo.setBackground(Color.red);
+        }
+        if(Source == this.VV.BtnVer || Source == this.VV.BtnOcultar){
+            this.VV.BtnSalir.setVisible(Source == this.VV.BtnVer);
+        }
+    }
+
+    public static int getRandom(int n, int m) {
+        if (n > m) {
+            throw new IllegalArgumentException("El mínimo no puede ser mayor que el máximo");
+        }
+        Random r = new Random();
+        // nextInt(limite) genera de 0 a limite-1. 
+        // Sumamos 1 para incluir m, y luego sumamos n para desplazar el rango.
+        return r.nextInt((m - n) + 1) + n;
+    }
+
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == this.VV){
+            this.VV.CalabazinContainer.setBounds(getRandom(64, 640 - 64), getRandom(64, 480 - 64), 64, 64);
+            this.VV.CalabazinContainer.setVisible(true);
+            this.VV.repaint();
+            this.VV.revalidate();
+        }
+    }
+
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == this.VV){
+            this.VV.CalabazinContainer.setVisible(false);
+            this.VV.repaint();
+            this.VV.revalidate();
+        }
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+
+    private void listeners(){
+        this.VV.BtnOcultar.addMouseListener(this);
+        this.VV.BtnSalir.addMouseListener(this);
+        this.VV.BtnVer.addMouseListener(this);
+        this.VV.BtnVerde.addMouseListener(this);
+        this.VV.BtnRojo.addMouseListener(this);
+        
+        this.VV.addMouseListener(this);
+
+        // Menus+
+    
+        this.VV.ItemSalir.addActionListener(this);
+        this.VV.ItemCambiarPosicion.addActionListener(this);
+        this.VV.ItemGuardar.addActionListener(this);
+        this.VV.ItemSalir.addActionListener(this);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        Object Source = e.getSource();
+        if(Source == this.VV.ItemSalir){
+            System.exit(0);
+        }
     }
 
 }
--- intento_gui/src/vista/VistaVentana.java
+++ intento_gui/src/vista/VistaVentana.java
@@ -1,22 +1,112 @@
 package vista;
 
 import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class VistaVentana extends JFrame {
+
+	// Botones
+	public JButton BtnSalir = new JButton("Salir");
+	public JButton BtnVerde = new JButton("Verde");
+	public JButton BtnRojo = new JButton("Rojo");
+	public JButton BtnVer = new JButton("Ver");
+	public JButton BtnOcultar = new JButton("Ocultar");
+
+	public ImageIcon Calabazin = new ImageIcon("calabazin.png");
+	public JLabel CalabazinContainer = new JLabel();
+
+	public JMenuBar BarraDeMenu = new JMenuBar();
+
+	public JMenu MenuArchivo = new JMenu("Archivo");
+	public JMenuItem ItemGuardar = new JMenuItem("Guardar Como");
+	public JMenuItem ItemSalir = new JMenuItem("Salir");
+
+	public JMenuItem MenuCalabazin = new JMenu("Calabazin");
+	public JMenuItem ItemMatar = new JMenuItem("Matarlo...");
+	public JMenuItem ItemCambiarPosicion = new JMenuItem("Cambiar Posicion");
+
+	public JMenuItem MenuAyuda = new JMenu("Ayuda");
+	public JMenuItem ItemAyuda = new JMenuItem("Ayuda...");
+	public JMenuItem ItemOpciones = new JMenuItem("Opciones");
+
+	public JMenuItem MenuEditar = new JMenu("Editar");
+	public JMenuItem ItemVerde = new JMenuItem("Verde...");
+	public JMenuItem ItemRojo = new JMenuItem("Rojo");
+
+	// Menus
+ 	final int W = 640;
+	final int H = 480;
+
 	
 	public VistaVentana() {
 		// Configuración de la ventana
 		this.setTitle("Mi Ventana");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setSize(640, 480);
+		this.setSize(W, H);
 		
 		this.getContentPane().setBackground(new Color(255, 192, 203));
 		this.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         this.setResizable(false);
         this.setAlwaysOnTop(true);
-        this.setEnabled(false);
+        this.setEnabled(true);
+		this.setLayout(null);
+		
+		this.CalabazinContainer.setIcon(Calabazin); 
+		this.CalabazinContainer.setBounds(300, 300, 64, 64);
+		this.add(this.CalabazinContainer);
+
+		botones();
+		menus();
+	}
+
+	private void botones(){
+		
+		this.BtnVerde.setBounds(30, 30, 120, 30);
+		this.BtnRojo.setBounds(W - 120 - 30, 30, 120, 30);
+		this.BtnSalir.setBounds(W / 2 - 60, H / 2 - 30, 120, 30);
+		this.BtnVer.setBounds(30, H - 60 - 30, 120, 30);
+		this.BtnOcultar.setBounds(W - 120 - 30, H - 60 - 30, 120, 30);
+
+		this.add(BtnVerde);
+		this.add(BtnRojo);
+		this.add(BtnSalir);
+		this.add(BtnVer);
+		this.add(BtnOcultar);
+	}
+
+	private void menus(){
+		this.MenuArchivo.add(this.ItemGuardar);
+		this.MenuArchivo.add(this.ItemSalir);
+
+		this.MenuCalabazin.add(this.ItemMatar);
+		this.MenuCalabazin.add(this.ItemCambiarPosicion);
+
+		this.MenuAyuda.add(this.ItemAyuda);
+		this.MenuAyuda.add(this.ItemOpciones);
+
+		this.MenuEditar.add(this.ItemVerde);
+		this.MenuEditar.add(this.ItemRojo);
+
+		this.BarraDeMenu.add(this.MenuArchivo);
+		this.BarraDeMenu.add(this.MenuCalabazin);
+		this.BarraDeMenu.add(this.MenuAyuda);
+		this.BarraDeMenu.add(this.MenuEditar);
+
+		this.ItemSalir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+
+		this.setJMenuBar(this.BarraDeMenu);
 	}
 }
